2a1ab9f08608ad81993ea11bb514ca00358d1a5d
Victor Veliz <ing-vj@outlook.com>
2019-07-09 21:11:00 -0400

    Tutorial 57 - Part 21 - Login View Model

--- Shop.UIForms/Shop.UIForms/ViewModel/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModel/LoginViewModel.cs
@@ -1,8 +1,57 @@
 ﻿namespace Shop.UIForms.ViewModel
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Xamarin.Forms;
 
     public class LoginViewModel
     {
         public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public LoginViewModel()
+        {
+            this.Email = "vveliz@bufalinda.com";
+            this.Password = "123123";
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email.",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password.",
+                    "Accept");
+                return;
+            }
+
+            if(!this.Email.Equals("vveliz@bufalinda.com") || !this.Password.Equals("123123"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "user or password wrong.",
+                    "Accept");
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Fuck yeah!",
+                    "Accept");
+            return;
+        }
     }
 }
